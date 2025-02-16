c4e11d2c07a84b895a05f4ec01ad656de2faedab
KgMoney38 <kodydavison@hotmail.com>
2025-02-15 19:06:02 -0600

    Deadline fixed

--- src/Deadline.java
+++ src/Deadline.java
@@ -1,2 +1,32 @@
-public class Deadline {
+import java.time.LocalDateTime;
+class Deadline extends Event implements Completable
+{
+    //Thank God for Intelli J I only had to type like 2 sentences in this class
+
+    private boolean complete;
+
+    public Deadline(String name, LocalDateTime deadline)
+    {
+        super(name, deadline);
+        this.complete = false;
+    }
+
+    @Override
+    public void complete()
+    {
+        this.complete = true;
+    }
+
+    @Override
+    public boolean isCompleted()
+    {
+        return complete;
+    }
+
+    @Override
+    public String getName()
+    {
+        return name;
+    }
+
 }
