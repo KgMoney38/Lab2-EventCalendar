5da3fe9ece9d34908aa59022c23d88bcb91ea7ef
KgMoney38 <kodydavison@hotmail.com>
2025-02-15 18:29:04 -0600

    previous version of Event is good

--- src/Event.java
+++ src/Event.java
@@ -1,15 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Event {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.time.LocalDateTime;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+abstract class Event implements Comparable<Event>
+{
+    protected String name;
+    protected LocalDateTime dateTime;
+
+    public Event(String name, LocalDateTime dateTime)
+    {
+        this.name = name;
+        this.dateTime = dateTime;
+    }
+
+    public abstract String getName();
+
+    public LocalDateTime getDateTime()
+    {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime)
+    {
+        this.dateTime = dateTime;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(Event e)
+    {
+        return this.dateTime.compareTo(e.getDateTime());
     }
 }
